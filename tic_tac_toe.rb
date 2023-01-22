a51c8044aedc625f0866312c2d2cf352582b2edf
Daniel Hamm <7501287+daniel-hamm@users.noreply.github.com>
2023-01-22 19:57:33 +0100

    Create empty game grid

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,6 +2,22 @@
 
 # Main game class
 class Game
+  def initialize
+    @grid = {}
+    self.new_game
+  end
+
+  def new_game
+    self.create_grid
+  end
+
+  def display_grid
+    @grid.each_with_index do |value, index|
+      print "#{value}\t"
+      print "\n" if index == 2 || index == 5
+    end
+  end
+
   def add_point
     @points += 1
   end
@@ -12,6 +28,17 @@ class Game
 
   def display_results
     "#{@name} has #{@points} points."
+  end
+
+  private
+
+  def create_grid
+    9.times do |i|
+      @grid[i] = {
+        player: '',
+        marker: ''
+      }
+    end
   end
 end
 
@@ -32,3 +59,5 @@ player1 = Player.new('Robert', 0)
 player2 = Player.new('Junior', 0)
 
 puts player1.display_results
+
+game.display_grid
